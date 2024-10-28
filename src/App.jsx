e8a9613344913070e17044f695c01ab9c9113452
Anay Saha <anaysahapaban@gmail.com>
2024-10-29 00:39:04 +0600

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  // eslint-disable-next-line no-empty-pattern, no-undef
+  const [] = useState(0)
+
+  return (
+    <>
+    
+      <h1 className='text-7xl bg-rose-600'>Vite + React</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
